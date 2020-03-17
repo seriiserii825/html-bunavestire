2ac97bfa2cfbec258c21aea06aefa1dc3aa5f99f
serii <seriiburduja@gmail.com>
2020-03-17 08:54:06 +0200

    search adaptive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,11 +3,13 @@ $(function () {
 		$('#js-search input').fadeIn();
 		$(this).fadeOut(10);
 		$('#js-close').fadeIn(200);
+		$('#js-search').css('width', '100%');
 	});
 	$('#js-close').on('click', function () {
 		$('#js-search input').fadeOut();
 		$(this).fadeOut(10);
 		$('#js-open').fadeIn(200);
+		$('#js-search').css('width', '10%');
 	});
 });
 
