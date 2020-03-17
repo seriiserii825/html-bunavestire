e91fe20a22b4c1b1797eb39bddbc966f347b5e7c
serii <seriiburduja@gmail.com>
2020-03-17 08:41:59 +0200

    search

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,14 @@
 $(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
-
+	$('#js-open').on('click', function () {
+		$('#js-search input').fadeIn();
+		$(this).fadeOut(10);
+		$('#js-close').fadeIn(200);
+	});
+	$('#js-close').on('click', function () {
+		$('#js-search input').fadeOut();
+		$(this).fadeOut(10);
+		$('#js-open').fadeIn(200);
+	});
 });
 
 
